ee5ec0bd144f5b7c72e8a086f3c142f4ea10acbf
Arpit Pandey <arp779921@gmail.com>
2025-06-30 10:18:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find index/find-index.py
@@ -0,0 +1,13 @@
+#User function Template for python3
+
+arr = tuple(map(int, input().split()))
+x = int(input())
+
+########### Write your code below ###############
+# Print the index of x in arr
+for i in range(len(arr)):
+    if arr[i] == x:
+        print(i)
+        break
+
+########### Write your code above ###############
