ff593f6f71a8fdb409001735bc76776f834946ee
Arpit Pandey <arp779921@gmail.com>
2025-07-03 22:49:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.py
+++ Difficulty: Basic/Reverse a String/reverse-a-string.py
@@ -1,5 +1,8 @@
 #User function Template for python3
+
 class Solution:
-    def revStr (self, s : str) -> str :
-        reverse = s[::-1]
-        return reverse
+     def reverseString(self, s: str) -> str:
+        res = ''
+        for i in range(len(s)-1,-1,-1):
+            res = res + s[i]
+        return res
