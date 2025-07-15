599c8a163abf0345ca0ab6229173f936797bcd52
Arpit Pandey <arp779921@gmail.com>
2025-07-15 14:57:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nonRepeatingChar(self,s):
+        out = {}
+        for char in s:
+            if char in out:
+                out[char] +=1
+            else:
+                out[char] =1
+        for key, value in out.items():
+            if value==1:
+                return key
+            else:
+                continue
+        return '$'
