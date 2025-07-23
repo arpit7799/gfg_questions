1c9884f61a0cfe7a0a111ef512d3ca19b25e1773
Arpit Pandey <arp779921@gmail.com>
2025-07-23 09:48:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Binary String/binary-string.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to count the number of substrings that start and end with 1.
+    def binarySubstring(self,n,s):
+        count = 0
+        for i in range(n):
+            if s[i] == '1':
+                count +=1
+        return (count * (count-1)) //2
