6475b12d7b70d3178bfc5b72c0992dc8f476adbb
Arpit Pandey <arp779921@gmail.com>
2025-08-08 10:06:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Taking input - Java/taking-input-java.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Implement Scanner Class
+        // Take all inputs
+        // Print all the values
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        float fraction = sc.nextFloat();
+        long number = sc.nextLong();
+        byte small = sc.nextByte();
+        sc.nextLine();
+        String Name = sc.nextLine();
+        System.out.println(age);
+        System.out.println(fraction);
+        System.out.println(number);
+        System.out.println(small);
+        System.out.println(Name);
+    }
+}
