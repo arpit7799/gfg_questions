1a90d9da40cd65eff7a77a42c55846ef0ff5c8b3
Arpit Pandey <arp779921@gmail.com>
2025-08-17 22:12:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Replace all 0's with 5/replace-all-0s-with-5.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int convertFive(int n) {
+        if (n == 0)
+            return 5;
+        int result = 0, place = 1;
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit == 0)
+                digit = 5;
+            result += digit * place;
+            place *= 10;
+            n /= 10;
+        }
+        return result;
+    }
+};
