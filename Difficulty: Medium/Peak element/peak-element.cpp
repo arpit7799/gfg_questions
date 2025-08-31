7d21597f690155b45f3604454ba309f9288979b6
Arpit Pandey <arp779921@gmail.com>
2025-08-31 11:01:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Peak element/peak-element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int peakElement(vector<int> &arr) {
+        int n = arr.size();
+        int left = 0, right = n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if ((mid == 0 || arr[mid] >= arr[mid - 1]) &&
+                (mid == n - 1 || arr[mid] >= arr[mid + 1])) {
+                return mid;
+            }
+            else if (mid > 0 && arr[mid - 1] > arr[mid]) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
