895cb6b21eac7bef3e7afc6343d01602c26da4fe
Arpit Pandey <arp779921@gmail.com>
2025-09-11 23:47:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ if-else (Decision Making)/c-ifelse-decision-making.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    std::string compareFive(int n) {
+        if (n > 5) {
+            return "Greater than 5";
+        } else if (n < 5) {
+            return "Less than 5";
+        } else {
+            return "Equal to 5";
+        }
+    }
+};
