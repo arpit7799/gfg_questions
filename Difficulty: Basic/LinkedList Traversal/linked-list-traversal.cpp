dd6b560d080fb897998d83c338ed82935741d2ba
Arpit Pandey <arp779921@gmail.com>
2026-04-04 09:59:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/LinkedList Traversal/linked-list-traversal.cpp
@@ -0,0 +1,24 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void printList(Node* head) {
+        // code here
+        Node* temp = head;
+        while(temp != NULL){
+            cout<< temp->data << " ";
+            temp = temp->next;
+        }
+    }
+};
