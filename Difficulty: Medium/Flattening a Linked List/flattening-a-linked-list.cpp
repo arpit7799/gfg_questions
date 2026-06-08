2b1235aeac550d44b59ddad6722cbf939aed048e
Arpit Pandey <arp779921@gmail.com>
2026-06-08 11:01:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -0,0 +1,52 @@
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* bottom;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+        bottom = nullptr;
+    }
+}; */
+
+class Solution {
+  public:
+    
+    Node* merge(Node* a, Node* b){
+        
+        if(a == NULL) return b;
+        if(b == NULL) return a;
+        
+        Node* result;
+        
+        if(a->data <= b->data){
+            result = a;
+            result->bottom = merge(a->bottom, b);
+        }
+        else{
+            result = b;
+            result->bottom = merge(a, b->bottom);
+        }
+        
+        result->next = NULL;
+        
+        return result;
+    }
+    
+    Node *flatten(Node *root) {
+        
+        if(root == NULL || root->next == NULL)
+            return root;
+        
+        // Flatten the right side
+        root->next = flatten(root->next);
+        
+        // Merge current list with flattened right list
+        root = merge(root, root->next);
+        
+        return root;
+    }
+};
